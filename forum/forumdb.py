5f0092cc9ccdd9b8a6c222dc7528d9c673a490b1
Franco Sarachu <fsarachu@gmail.com>
2016-11-23 14:05:54 -0300

    Convert docstring singlequotes to doublequotes

--- forum/forumdb.py
+++ forum/forumdb.py
@@ -10,13 +10,13 @@ DB = psycopg2.connect("dbname='forum'")
 
 ## Get posts from database.
 def GetAllPosts():
-    '''Get all the posts from the database, sorted with the newest first.
+    """Get all the posts from the database, sorted with the newest first.
 
     Returns:
       A list of dictionaries, where each dictionary has a 'content' key
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
-    '''
+    """
     cursor = DB.cursor()
     cursor.execute("""SELECT content, time FROM posts ORDER BY time DESC""")
     posts = [{'content': str(row[0]), 'time': str(row[1])} for row in cursor.fetchall()]
@@ -25,10 +25,10 @@ def GetAllPosts():
 
 ## Add a post to the database.
 def AddPost(content):
-    '''Add a new post to the database.
+    """Add a new post to the database.
 
     Args:
       content: The text content of the new post.
-    '''
+    """
     cursor = DB.cursor()
     cursor.execute('INSERT INTO posts (content) VALUES ({})'.format(content))
