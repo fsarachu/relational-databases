b98fb9ffd1b9482ca9d59b92670865cc372c62cb
Franco Sarachu <fsarachu@gmail.com>
2016-11-26 18:39:01 -0300

    Implement reportMatch() function

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -78,6 +78,11 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute('INSERT INTO matches (winner, loser) VALUES (%s, %s)', (winner, loser))
+    conn.commit()
+    conn.close()
 
 
 def swissPairings():
